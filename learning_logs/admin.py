437a7881d61154a626a1c531dee3d5d2b914c79a
Gabriel Rodrigues <ogabrielrdgs@gmail.com>
2023-08-16 18:51:18 -0300

    Topic and entry models have been defined

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("title", "date_added", "public", "owner")
+    list_filter = ("public", "date_added")
+    search_fields = ("title", "content")
+    ordering = ("-date_added",)
+
+
+@admin.register(models.Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ("content", "date_added", "topic")
+    list_filter = (
+        "topic",
+        "date_added",
+    )
+    search_fields = ("content",)
+    ordering = ("-date_added",)
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(_("title"), max_length=264)
+    date_added = models.DateTimeField(_("date added"), auto_now_add=True)
+    public = models.BooleanField(_("public"))
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="topics",
+        on_delete=models.CASCADE,
+        verbose_name=_("owner"),
+    )
+
+    def __str__(self):
+        return self.title + " - " + self.owner.email
+
+    class Meta:
+        verbose_name = _("topic")
+        verbose_name_plural = _("topics")
+        ordering = ("-date_added",)
+        constraints = (
+            models.UniqueConstraint(
+                fields=["owner", "title"],
+                name="unique_topic_title",
+                violation_error_message=_("You already have a topic with this title!"),
+            ),
+        )
+
+
+class Entry(models.Model):
+    content = models.TextField(_("content"))
+    date_added = models.DateTimeField(_("date added"), auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic, related_name="entries", on_delete=models.CASCADE, verbose_name=_("topic")
+    )
+
+    class Meta:
+        verbose_name = _("entry")
+        verbose_name_plural = _("entries")
+        ordering = ("-date_added",)
+
+    def __str__(self):
+        if len(self.content) > 50:
+            return self.content[:50] + "..."
+        else:
+            return self.content
