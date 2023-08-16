e7746d20081408abb2f208469dd186b7128fd004
Gabriel Rodrigues <ogabrielrdgs@gmail.com>
2023-08-16 11:56:33 -0300

    Learning logs app development started

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
+    path("", include("learning_logs.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
